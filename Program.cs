d3250198dfe9906cdfa76e0051a7cebd45dc770e
Przemyslaw Walkowski <przemyslaw.walkowski@snpgroup.com>
2020-08-14 08:21:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OMSDataManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
